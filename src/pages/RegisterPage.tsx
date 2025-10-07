6646fab8b56c6605c87ccb9f42755b6285a2b5d4
camilo.medinarivera <camilo.medinarivera@plexus.tech>
2025-10-07 12:24:51 +0200

    [feature/fix-integration] another error

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
+import type { FormEvent } from 'react';
 import { registerUser } from '../api';
 import './RegisterPage.css';
 
